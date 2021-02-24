49b9f594a7f91e35864a4d8ad85ef64e450dcbb9
Nikita78031 <Niki503@yandex.ru>
2021-02-24 23:53:12 +0300

    add

---
+++ cub3d.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwanetta <dwanetta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/19 16:33:24 by dwanetta          #+#    #+#             */
+/*   Updated: 2021/02/24 20:31:52 by dwanetta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include <stdio.h>
+
+int	ft_pars(fd)
+{
+	t_file file;
+	int i;
+
+	i = 1;
+	file.R_x = 0;
+	file.R_y = 0;
+	file.NO_texture = NULL;
+	file.SO_texture = NULL;
+	file.WE_texture = NULL;
+	file.EA_texture = NULL;
+	file.S_texture = NULL;
+	file.C[0] = 0;
+	file.C[0] = 0;
+	file.map = NULL;
+	while (i > 0)
+	{
+		if (i == -1)
+			break ;
+	}
+}
+
+int ft_open_file(int argc, char *argv[])
+{
+	int fd;
+	int size_argv;
+
+	if (argc <= 1)
+	{
+		ft_putstr_fd("Error\nNumber of arguments\n", 1);
+		return (-1);
+	}
+	size_argv = ft_strlen(argv[1]) - 1;
+	if (argv[1][size_argv - 3] != '.' || argv[1][size_argv - 2] != 'c'
+	|| argv[1][size_argv - 1] != 'u' || argv[1][size_argv] != 'b')
+	{
+		ft_putstr_fd("Error\nInvalid file extension\n", 1);
+		return (-1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	return (ft_pars(fd));
+}
+
+int main(int argc, char *argv[])
+{
+
+	if (ft_open_file(argc, argv) == -1)
+		exit(-1);
+}
+
+/*
+
+void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}*/
+/*
+	while (x != 100)
+	{
+		y = (149*x-50) / 99;
+		mlx_pixel_put(mlx, mlx_win, x, y, 0x00ff0000);
+		x++;
+	}
+*/
---
+++ cub3d.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwanetta <dwanetta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 18:41:07 by dwanetta          #+#    #+#             */
+/*   Updated: 2021/02/24 20:28:11 by dwanetta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include "mlx_linux/mlx.h"
+//#include "mlx_mac/mlx.h"
+#include "libft/libft.h"
+#include "get_next_line/get_next_line.h"
+#include <math.h>
+#include <fcntl.h>
+
+#define FOV 3.14 / 3
+
+typedef struct	s_data
+{
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_data;
+
+typedef struct	s_file
+{
+	int			R_x;
+	int			R_y;
+	char		*NO_texture;
+	char		*SO_texture;
+	char		*WE_texture;
+	char		*EA_texture;
+	char		*S_texture;
+	int			F[3];
+	int			C[3];
+	char		*map;
+}				t_file;
+
+#endif
