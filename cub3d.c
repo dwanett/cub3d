8d817d0184801c3ec53bba8fd07979454413bb94
Nikita78031 <Niki503@yandex.ru>
2021-03-14 14:39:41 +0300

    add

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: dwanetta <dwanetta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 16:33:24 by dwanetta          #+#    #+#             */
-/*   Updated: 2021/03/13 15:36:31 by dwanetta         ###   ########.fr       */
+/*   Updated: 2021/03/09 20:17:56 by dwanetta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 int	ft_close(int keycode, t_vars *vars)
 {
-	if (keycode == 65307 || keycode == 53)
+	if (keycode == 65307)
 	{
 		mlx_destroy_window(vars->mlx, vars->win);
 		exit(-1);
@@ -64,7 +64,7 @@ int ft_window(t_file file)
 								 &img.endian);
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_hook(vars.win, 2, 1L<<0, ft_close, &vars);
+	mlx_hook(vars.win, 2, 1L << 0, ft_close, &vars);
 	mlx_hook(vars.win, 33, 1L << 5, ft_close_ex, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
